fea8bb26f6d41d091332b8bd431fe84b212c8a0d
Vikas  Tank <vikas.tank@Yash.com>
2025-01-29 12:55:55 +0530

    status code updated to category/product/order

--- src/services/category/controller.py
+++ src/services/category/controller.py
@@ -4,6 +4,9 @@ from src.config.error_constants import ErrorMessage
 from src.services.category.model import CategoryModel
 from src.services.category.serializers import CategoryAddInBound, CategoryAddOutBound, CategoryUpdateInBound
 from src.utils.common_serializers import CommonMessageOutbound
+from fastapi.responses import JSONResponse
+from fastapi import status
+from fastapi.encoders import jsonable_encoder
 
 
 class CategoryController:
@@ -14,10 +17,10 @@ class CategoryController:
         if payload.name:
             data = CategoryModel.get(name=payload.name)
             if data:
-                return CommonMessageOutbound(message=ErrorMessage.RECORD_ALREADY_EXISTS.format("Category"))
+                return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=jsonable_encoder(CommonMessageOutbound(message=ErrorMessage.RECORD_ALREADY_EXISTS.format("Category"))))
         category = CategoryModel.create(**payload.__dict__)
         data = CategoryAddOutBound(**category.__dict__)
-        response = CommonMessageOutbound(message=ErrorMessage.CREATED_SUCCESSFULLY, data=data.__dict__)
+        response = JSONResponse(status_code=status.HTTP_201_CREATED, content=jsonable_encoder(CommonMessageOutbound(message=ErrorMessage.CREATED_SUCCESSFULLY, data=data.__dict__)))
         return response
 
     @classmethod
@@ -26,13 +29,13 @@ class CategoryController:
 
         data = CategoryModel.get(_id=_id, page=page, size=size)
         if not data:
-            return CommonMessageOutbound(message=ErrorMessage.RECORD_NOT_FOUND)
+            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=jsonable_encoder(CommonMessageOutbound(message=ErrorMessage.RECORD_NOT_FOUND)))
         if _id:
-            result = CategoryAddOutBound(**data.__dict__).__dict__
+            result = CategoryAddOutBound(**data.__dict__)
         else:
             result = [CategoryAddOutBound(**category.__dict__) for category in data]
 
-        return CommonMessageOutbound(data=result)
+        return JSONResponse(status_code=status.HTTP_200_OK, content=jsonable_encoder(CommonMessageOutbound(message="success", data=result)))
 
     @classmethod
     async def delete_category(cls, _id: int):
@@ -40,20 +43,20 @@ class CategoryController:
 
         category = CategoryModel.get(_id=_id)
         if not category:
-            return CommonMessageOutbound(message=ErrorMessage.RECORD_NOT_FOUND)
+            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=jsonable_encoder(CommonMessageOutbound(message=ErrorMessage.RECORD_NOT_FOUND)))
         CategoryModel.patch(_id=_id, **{"status": CategoriesStatusConstant.Inactive})
-        return CommonMessageOutbound(message=ErrorMessage.RECORD_DELETED_SUCCESSFULLY)
+        return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=jsonable_encoder(CommonMessageOutbound(message=ErrorMessage.RECORD_DELETED_SUCCESSFULLY)))
 
     @classmethod
     async def category_update(cls, payload: CategoryUpdateInBound):
         """category update function"""
         if not payload.name and not payload.description:
-            return CommonMessageOutbound(message=ErrorMessage.MISSING_FILED_TO_UPDATE)
+            return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=jsonable_encoder(CommonMessageOutbound(message=ErrorMessage.MISSING_FILED_TO_UPDATE)))
         payload_dict = payload.dict(exclude_unset=True, exclude_none=True)
         category = CategoryModel.get(_id=payload.id)
         if not category:
-            return CommonMessageOutbound(message=ErrorMessage.RECORD_NOT_FOUND)
+            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=jsonable_encoder(CommonMessageOutbound(message=ErrorMessage.RECORD_NOT_FOUND)))
 
         data = CategoryModel.patch(_id=payload.id, **payload_dict)
         data = CategoryAddOutBound(**data.__dict__)
-        return CommonMessageOutbound(message=ErrorMessage.RECORD_UPDATED_SUCCESSFULLY, data=data.__dict__)
+        return JSONResponse(status_code=status.HTTP_200_OK, content=jsonable_encoder(CommonMessageOutbound(message=ErrorMessage.RECORD_UPDATED_SUCCESSFULLY, data=data.__dict__)))
